e9df54ca190168193964f09063cd9e8800f6a988
Misagh Momeni Bashusqeh <166559069+MisaghMomeniB@users.noreply.github.com>
2025-04-28 12:00:43 +0330

    logout and dashboard views

--- auth_system/accounts/views.py
+++ auth_system/accounts/views.py
@@ -24,3 +24,10 @@ def login_view(request):
     else:
         form = CustomAuthenticationForm()
     return render(request, 'accounts/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+def dashboard_view(request):
+    return render(request, 'accounts/dashboard.html')
