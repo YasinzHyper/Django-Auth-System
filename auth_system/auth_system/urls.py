e770f6f7f5bd0cc33134890323493eb75b9faa29
Misagh Momeni Bashusqeh <166559069+MisaghMomeniB@users.noreply.github.com>
2025-04-28 12:04:31 +0330

    add dashboard url

--- auth_system/auth_system/urls.py
+++ auth_system/auth_system/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    # path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
 ]
